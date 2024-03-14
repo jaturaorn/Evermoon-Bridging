9bd2055237f582c0765a065ceedd99b65e66a92e
Jaturaporn Sutongkeaw <Jaturapornff@gamil.com>
2024-03-14 10:17:22 +0700

    edit some banner , finished in navbar component

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -2,11 +2,13 @@ const Banner = () => {
   return (
     <main className="w-full h-[500px]">
       <div
-        className="flex justify-center h-full items-center uppercase text-main-1
-      font-bold text-[60px] bg-[url('/environment1.png')] bg-no-repeat bg-cover
-      bg-opacity-70"
+        className="flex justify-center h-full items-center 
+       bg-[url('/environment1.png')] bg-no-repeat bg-center
+      border-main-1 border-y-2"
       >
-        bridge
+        <p className="font-bold text-[60px] [text-shadow:_2px_1px_5px_rgb(0_0_0_/_70%)] uppercase text-main-1">
+          bridge
+        </p>
       </div>
     </main>
   );
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,35 @@
+import Image from "next/image";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex justify-between p-2 mx-auto">
+      <div className="flex items-center gap-x-4">
+        <Image
+          src="/LogoEvermoon_Primary.png"
+          width={300}
+          height={300}
+          alt=""
+          className="w-[120px]"
+        />
+        <h2 className="capitalize text-xl text-white">bridge</h2>
+      </div>
+      <div className="flex items-center">
+        <button
+          className="flex items-center px-[20px] py-[12px] bg-[#372C7D] rounded-lg text-lg
+        gap-x-2 border-2 border-white text-white"
+        >
+          <Image
+            src="/metamask.png"
+            width={300}
+            height={300}
+            alt=""
+            className="w-[30px]"
+          />
+          Connect Wallet
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
